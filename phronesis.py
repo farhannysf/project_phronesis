7877a5f85556a0dce6e87826d8947b3e7404513e
Penske Williano <kaytjondro@yahoo.com>
2015-06-01 01:31:50 +0700

    Update phronesis.py v 0.0.3
    - Added session files (trial)

--- phronesis.py
+++ phronesis.py
@@ -1,20 +1,33 @@
 import aiml
 import os
-import os.path 
+import os.path
+import marshal 
 
 AI = aiml.Kernel()
-if 	os.path.isfile("standard.brn"):
+session = AI.getSessionData("Bob")
+if os.path.isfile("standard.brn"):
 	print 'Loading brain file...'
 	print 'Please wait a moment...'
 	AI.bootstrap(brainFile = "standard.brn")
+	
+	sessionFile = file("Bob.ses", "wb")
+	marshal.dump(session, sessionFile)
+	sessionFile.close()
+
+	sessionFile = file("Bob.ses", "rb")
+	session = marshal.load(sessionFile)
+	sessionFile.close()
+	for pred,value in session.items():
+		AI.setPredicate(pred, value, "Bob")
+    
 
 else:	AI.setPredicate("secure", "yes")
-	AI.bootstrap(learnFiles="startup.xml")
-	AI.respond("LOAD AIML B")
-	AI.saveBrain("standard.brn")
+AI.bootstrap(learnFiles="startup.xml")
+AI.respond("LOAD AIML B")
+AI.saveBrain("standard.brn")
 
 print '==================================================='
-print '    Welcome to Phronesis chatbot version 0.0.2'
+print '    Welcome to Phronesis chatbot version 0.0.3'
 print '  Please start typing to begin your conversation.'
 print '==================================================='
 print ''
